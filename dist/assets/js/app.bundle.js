c1ee0fae0f2219f1af0d264887bf959dbc468c07
Miguel Alejandro <admin@miguelvinan.com>
2016-10-12 17:37:28 -0500

    Structure and styles for all template

--- dist/assets/js/app.bundle.js
+++ dist/assets/js/app.bundle.js
@@ -52,8 +52,15 @@
 	$(document).on('ready', function () {
 	  'use strict';
 	
-	  var $items = $('.Menu .List .List-item');
-	  var $listContainer = $('.ListContainer');
+	  var $items = $('.Menu .List .List-item'),
+	      $listContainer = $('.ListContainer'),
+	      $contactForm = $('.Contact-form'),
+	      $fieldInput = $contactForm.find('.field input, textarea');
+	
+	  var color = {
+	    red: '#FE2851',
+	    black: '#242424'
+	  };
 	
 	  $('.button.hamburguer').click(function () {
 	    $listContainer.toggleClass('is-active');
@@ -69,6 +76,12 @@
 	    } else {
 	      $items.removeClass('is-showing');
 	    }
+	  });
+	
+	  $fieldInput.focusin(function () {
+	    $(this).siblings('label').css({ color: color.red });
+	  }).focusout(function () {
+	    $(this).siblings('label').css({ color: color.black });
 	  });
 	});
 
--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,8 +1,15 @@
 $(document).on('ready', function(){
   'use strict'
 
-  var $items = $('.Menu .List .List-item')
-  var $listContainer = $('.ListContainer')
+  var $items = $('.Menu .List .List-item'),
+      $listContainer = $('.ListContainer'),
+      $contactForm = $('.Contact-form'),
+      $fieldInput = $contactForm.find('.field input, textarea')
+
+  var color = {
+    red: '#FE2851',
+    black: '#242424'
+  }
 
   $('.button.hamburguer').click( function() {
     $listContainer.toggleClass('is-active')
@@ -20,6 +27,10 @@ $(document).on('ready', function(){
     }
 
   });
+
+  $fieldInput
+    .focusin(function(){ $(this).siblings('label').css({color: color.red }) })
+    .focusout(function(){ $(this).siblings('label').css({color: color.black }) })
 
 
 });
